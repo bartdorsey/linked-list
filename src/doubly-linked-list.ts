c1389d61376d3534cfa48a3672cd6c17f22d255c
Bart Dorsey <bart@bartdorsey.com>
2023-09-14 17:06:07 -0500

    Updating to use ? instead of my custom optional type

--- src/doubly-linked-list.ts
+++ src/doubly-linked-list.ts
@@ -1,201 +1,206 @@
-
 export namespace DoublyLinkedList {
-    interface Node<T> {
-        value: T,
-        next: Optional<Node<T>>
-        prev: Optional<Node<T>>
+  interface Node<T> {
+    value: T;
+    next?: Node<T>;
+    prev?: Node<T>;
+  }
+
+  interface LinkedList<T> {
+    head?: Node<T>;
+    tail?: Node<T>;
+    length: number;
+  }
+
+  // Creates a node object
+  export function createNode<T>(
+    value: T,
+    next?: Node<T>,
+    prev?: Node<T>,
+  ): Node<T> {
+    return {
+      value,
+      next,
+      prev,
+    };
+  }
+
+  // Creates a list object
+  export function createLinkedList<T>(): LinkedList<T> {
+    return {
+      length: 0,
+    };
+  }
+
+  export function findNodeAtIndex<T>(
+    list: LinkedList<T>,
+    index: number,
+  ): Node<T> | undefined {
+    // Start at head
+    let currentNode = list.head;
+    // Loop through the list
+    for (let currentIndex = 0; currentIndex < index; currentIndex++) {
+      // if there's no currentNode, return undefined because
+      // we've hit the end of the list
+      if (!currentNode?.next) {
+        return;
+      }
+      // Change currentNode to the next node
+      currentNode = currentNode.next;
     }
+    // If we made it here we return the currentNode
+    return currentNode;
+  }
 
-    interface LinkedList<T> {
-        head: Optional<Node<T>>
-        tail: Optional<Node<T>>
-        length: number
+  export function getValueAtIndex<T>(
+    list: LinkedList<T>,
+    index: number,
+  ): T | undefined {
+    const node = findNodeAtIndex(list, index);
+    if (!node) {
+      return;
     }
+    return node.value;
+  }
 
-
-    // Creates a node object
-    export function createNode<T>(
-        value: T,
-        next: Optional<Node<T>> = null,
-        prev: Optional<Node<T>> = null): Node<T> {
-        return {
-            value,
-            next,
-            prev,
-        }
+  export function listToArray<T>(list: LinkedList<T>): T[] {
+    const arr: T[] = [];
+    let currentNode = list.head;
+    while (currentNode) {
+      arr.push(currentNode.value);
+      currentNode = currentNode.next;
     }
+    return arr;
+  }
 
-    // Creates a list object
-    export function createLinkedList<T>(): LinkedList<T> {
-        return {
-            head: null,
-            tail: null,
-            length: 0,
-        }
+  export function addValueToHead<T>(list: LinkedList<T>, value: T): void {
+    // Create a new node
+    const newNode = createNode(value);
+
+    if (list.length === 0) {
+      list.head = newNode;
+      list.tail = newNode;
+    } else {
+      // Set newNode's next property to the current head
+      newNode.next = list.head;
+      if (list.head) {
+        // set the current head's previous property to the new node
+        list.head.prev = newNode;
+      }
+      // Set the head to the new node
+      list.head = newNode;
+      // If there's one node, then the tail will be also set
+      if (list.length === 1) {
+        list.tail = newNode;
+      }
     }
+    // Increase the size of the list
+    list.length++;
+  }
 
-    export function findNodeAtIndex<T>(list: LinkedList<T>, index: number): Optional<Node<T>> {
-        // Start at head
-        let currentNode = list.head
-        // Loop through the list
-        for (let currentIndex = 0; currentIndex < index; currentIndex++) {
-            // if there's no currentNode, return null because
-            // we've hit the end of the list
-            if (!currentNode?.next) {
-                return null
-            }
-            // Change currentNode to the next node
-            currentNode = currentNode.next
-        }
-        // If we made it here we return the currentNode
-        return currentNode
+  export function addValueToTail<T>(list: LinkedList<T>, value: T): void {
+    // Create a new Node
+    const newNode = createNode(value);
+    // If the list is empty just set the new node to be both
+    // the head and the tail
+    if (list.length === 0) {
+      list.head = newNode;
+      list.tail = newNode;
+    } else {
+      if (list.tail) {
+        // Set the current tail's next property to our new node
+        list.tail.next = newNode;
+      }
+      // Set our new node's previous property to the current tail
+      newNode.prev = list.tail;
+      // set the tail property to the newNode
+      list.tail = newNode;
     }
+    // Increase the size of the list
+    list.length++;
+  }
 
-    export function getValueAtIndex<T>(list: LinkedList<T>, index: number): Optional<T> {
-        const node = findNodeAtIndex(list, index)
-        if (!node) {
-            return null
-        }
-        return node.value
+  export function removeFromHead<T>(list: LinkedList<T>): T | undefined {
+    // If the list is empty do nothing
+    if (list.length === 0) {
+      return;
     }
-
-    export function listToArray<T>(list: LinkedList<T>): T[] {
-        const arr: T[] = []
-        let currentNode = list.head
-        while (currentNode) {
-            arr.push(currentNode.value)
-            currentNode = currentNode.next
-        }
-        return arr
+    if (!list.head) {
+      return;
     }
-
-    export function addValueToHead<T>(list: LinkedList<T>, value: T): void {
-        // Create a new node
-        const newNode = createNode(value)
-
-        if (list.length === 0) {
-            list.head = newNode
-            list.tail = newNode
-        } else {
-            // Set newNode's next property to the current head
-            newNode.next = list.head
-            if (list.head) {
-                // set the current head's previous property to the new node
-                list.head.prev = newNode
-            }
-            // Set the head to the new node
-            list.head = newNode
-            // If there's one node, then the tail will be also set
-            if (list.length === 1) {
-                list.tail = newNode
-            }
-        }
-        // Increase the size of the list
-        list.length++
+    // grab the value from the current head
+    const value = list.head.value;
+    // If there's only one node
+    if (list.length === 1) {
+      // set head and tail to undefined
+      list.head = undefined;
+      list.tail = undefined;
+      // Otherwise we need to make next the new head
+    } else {
+      // Set the head to be the next node
+      list.head = list.head.next;
+      if (list.head) {
+        // Set the new head's prev to undefined
+        list.head.prev = undefined;
+      }
+      if (list.length === 2) {
+        list.tail = list.head;
+      }
     }
+    // Decrement the length
+    list.length--;
+    // Return the value
+    return value;
+  }
 
-    export function addValueToTail<T>(list: LinkedList<T>, value: T): void {
-        // Create a new Node
-        const newNode = createNode(value)
-        // If the list is empty just set the new node to be both
-        // the head and the tail
-        if (list.length === 0) {
-            list.head = newNode
-            list.tail = newNode
-        } else {
-            if (list.tail) {
-                // Set the current tail's next property to our new node
-                list.tail.next = newNode
-            }
-            // Set our new node's previous property to the current tail
-            newNode.prev = list.tail
-            // set the tail property to the newNode
-            list.tail = newNode
-        }
-        // Increase the size of the list
-        list.length++
+  export function removeFromTail<T>(list: LinkedList<T>): T | undefined {
+    // If the list is empty do nothing
+    if (list.length === 0) {
+      return;
     }
-
-    export function removeFromHead<T>(list: LinkedList<T>): Optional<T> {
-        // If the list is empty do nothing
-        if (list.length === 0) {
-            return null
-        }
-        if (!list.head) {
-            return null
-        }
-        // grab the value from the current head
-        const value = list.head.value
-        // If there's only one node
-        if (list.length === 1) {
-            // set head and tail to null
-            list.head = null
-            list.tail = null
-            // Otherwise we need to make next the new head
-        } else {
-            // Set the head to be the next node
-            list.head = list.head.next
-            if (list.head) {
-                // Set the new head's prev to null
-                list.head.prev = null
-            }
-            if (list.length === 2) {
-                list.tail = list.head
-            }
-        }
-        // Decrement the length
-        list.length--
-        // Return the value
-        return value
+    // If the list's length is 1, we need to remove the head as well
+    if (list.length === 1) {
+      list.head = undefined;
     }
-
-    export function removeFromTail<T>(list: LinkedList<T>): Optional<T> {
-        // If the list is empty do nothing
-        if (list.length === 0) {
-            return null
-        }
-        // If the list's length is 1, we need to remove the head as well
-        if (list.length === 1) {
-            list.head = null
-        }
-        if (!list.tail) {
-            return null
-        }
-        // Grab the value from the list.tail
-        const value = list.tail.value
-        // Set the tail to be the node previous to the current tail
-        list.tail = list.tail.prev
-        // Decrement the length
-        list.length--
-        return value
+    if (!list.tail) {
+      return;
     }
+    // Grab the value from the list.tail
+    const value = list.tail.value;
+    // Set the tail to be the node previous to the current tail
+    list.tail = list.tail.prev;
+    // Decrement the length
+    list.length--;
+    return value;
+  }
 
-
-
-    export function insertValueAtIndex<T>(list: LinkedList<T>, index: number, value: T): null {
-        // if the list's length is less than the index, just return null
-        if (list.length < index) {
-            return null
-        }
-        // If the index is 0,
-        // Just use our Head function
-        if (index === 0) {
-            addValueToHead(list, value)
-            return null
-        }
-        // If the index is the last index,
-        // just use our tail function
-        if (index === list.length - 1) {
-            addValueToTail(list, value)
-        }
-        const node = findNodeAtIndex(list, index - 1)
-        if (!node) {
-            return null;
-        }
-        const newNode = createNode(value)
-        newNode.next = node.next
-        node.next = newNode
-        list.length++
-        return null
+  export function insertValueAtIndex<T>(
+    list: LinkedList<T>,
+    index: number,
+    value: T,
+  ): void {
+    // if the list's length is less than the index, just return undefined
+    if (list.length < index) {
+      return;
     }
+    // If the index is 0,
+    // Just use our Head function
+    if (index === 0) {
+      addValueToHead(list, value);
+      return;
+    }
+    // If the index is the last index,
+    // just use our tail function
+    if (index === list.length - 1) {
+      addValueToTail(list, value);
+    }
+    const node = findNodeAtIndex(list, index - 1);
+    if (!node) {
+      return;
+    }
+    const newNode = createNode(value);
+    newNode.next = node.next;
+    node.next = newNode;
+    list.length++;
+    return;
+  }
 }
--- src/singly-linked-list.ts
+++ src/singly-linked-list.ts
@@ -1,185 +1,192 @@
 export namespace SinglyLinkedList {
-    interface Node<T> {
-        value: T,
-        next: Optional<Node<T>>
+  interface Node<T> {
+    value: T;
+    next?: Node<T>;
+  }
+
+  interface LinkedList<T> {
+    head?: Node<T>;
+    length: number;
+  }
+
+  // Creates a node object
+  export function createNode<T>(value: T, next?: Node<T>): Node<T> {
+    return {
+      value,
+      next,
+    };
+  }
+
+  // Creates a list object
+  export function createLinkedList<T>(): LinkedList<T> {
+    const list: LinkedList<T> = {
+      length: 0,
+    };
+    return list;
+  }
+
+  export function findNodeAtIndex<T>(
+    list: LinkedList<T>,
+    index: number,
+  ): Node<T> | undefined {
+    // We start at the head
+    let currentNode = list.head;
+    // Loop through the node, until we get to index
+    for (let currentIndex = 0; currentIndex < index; currentIndex++) {
+      // if there's no currentNode.next, return undefined because
+      // we've hit the end of the list
+      if (!currentNode?.next) {
+        return;
+      }
+      // Change currentNode to the next node
+      currentNode = currentNode.next;
     }
+    // If we made it here we return the currentNode
+    return currentNode;
+  }
 
-    interface LinkedList<T> {
-        head: Optional<Node<T>>
-        length: number
+  export function getValueAtIndex<T>(
+    list: LinkedList<T>,
+    index: number,
+  ): T | undefined {
+    const node = findNodeAtIndex(list, index);
+    if (node != undefined) {
+      return node?.value;
+    } else {
+      return;
     }
+  }
 
-    // Creates a node object
-    export function createNode<T>(value: T, next: Optional<Node<T>> = null): Node<T> {
-        return {
-            value,
-            next
-        }
+  export function listToArray<T>(list: LinkedList<T>): T[] {
+    const arr: T[] = [];
+    let currentNode = list.head;
+    while (currentNode) {
+      arr.push(currentNode.value);
+      currentNode = currentNode.next;
     }
+    return arr;
+  }
 
-    // Creates a list object
-    export function createLinkedList<T>(): LinkedList<T> {
-        const list: LinkedList<T> = {
-            head: null,
-            length: 0
-        }
-        return list;
+  export function addValueToHead<T>(list: LinkedList<T>, value: T): void {
+    // Create a new node
+    const newNode = createNode(value);
+
+    if (list.length === 0) {
+      list.head = newNode;
+    } else {
+      // Set newNode's next property to the current head
+      newNode.next = list.head;
+      // Set the head to the new node
+      list.head = newNode;
     }
+    // Increase the size of the list
+    list.length++;
+  }
 
-    export function findNodeAtIndex<T>(list: LinkedList<T>, index: number): Optional<Node<T>> {
-        // We start at the head
-        let currentNode = list.head
-        // Loop through the node, until we get to index
-        for (let currentIndex = 0; currentIndex < index; currentIndex++) {
-            // if there's no currentNode.next, return null because
-            // we've hit the end of the list
-            if (!currentNode?.next) {
-                return null
-            }
-            // Change currentNode to the next node
-            currentNode = currentNode.next
-        }
-        // If we made it here we return the currentNode
-        return currentNode
+  export function addValueToTail<T>(list: LinkedList<T>, value: T): void {
+    const newNode = createNode(value);
+    // if the list is empty set the node
+    // to be the head
+    if (list.length === 0) {
+      list.head = newNode;
+    } else {
+      // We have to look for the last node.
+      // We can use our find function for this.
+      const tail = findNodeAtIndex(list, list.length - 1);
+      // If there's no tail, just return undefined
+      if (!tail) return;
+      tail.next = newNode;
     }
+    list.length++;
+    return;
+  }
 
-    export function getValueAtIndex<T>(list: LinkedList<T>, index: number): Optional<T> {
-        const node = findNodeAtIndex(list, index)
-        if (node != null) {
-            return node?.value
-        }
-        else {
-            return null
-        }
+  export function removeFromHead<T>(list: LinkedList<T>): T | undefined {
+    // If the list is empty do nothing
+    if (list.length === 0) {
+      return;
     }
-
-
-    export function listToArray<T>(list: LinkedList<T>): T[] {
-        const arr: T[] = []
-        let currentNode = list.head
-        while (currentNode) {
-            arr.push(currentNode.value)
-            currentNode = currentNode.next
-        }
-        return arr
+    if (!list.head) {
+      return;
     }
-
-    export function addValueToHead<T>(list: LinkedList<T>, value: T): void {
-        // Create a new node
-        const newNode = createNode(value)
-
-        if (list.length === 0) {
-            list.head = newNode
-        } else {
-            // Set newNode's next property to the current head
-            newNode.next = list.head
-            // Set the head to the new node
-            list.head = newNode
-        }
-        // Increase the size of the list
-        list.length++
+    // grab the value from the current head
+    const value = list.head.value;
+    // If there's only one node
+    if (list.length === 1) {
+      // set head to undefined
+      list.head = undefined;
+      // Otherwise we need to make next the new head
+    } else {
+      // Set the head to be the next node
+      list.head = list.head.next;
     }
+    // Decrement the length
+    list.length--;
+    // Return the value
+    return value;
+  }
 
-    export function addValueToTail<T>(list: LinkedList<T>, value: T): null {
-        const newNode = createNode(value);
-        // if the list is empty set the node
-        // to be the head
-        if (list.length === 0) {
-            list.head = newNode;
-        } else {
-            // We have to look for the last node.
-            // We can use our find function for this.
-            const tail = findNodeAtIndex(list, list.length - 1)
-            // If there's no tail, just return null
-            if (!tail) return null
-            tail.next = newNode
-        }
-        list.length++;
-        return null
+  export function removeFromTail<T>(list: LinkedList<T>): T | undefined {
+    // if there's no head, just return undefinded
+    if (!list.head) {
+      return;
     }
+    let value: T | undefined = undefined;
+    // If there's no next node
+    // We are removing the only node
+    if (!list.head.next) {
+      // grab the value from the head
+      value = list.head.value;
+      // and set head to undefined
+      list.head = undefined;
+      // Otherwise we need to iterate to find the tail
+    } else {
+      // We can use our findNodeAtIndex here
+      // To look for the next to last node
+      const nextToLastNode = findNodeAtIndex(list, list.length - 2);
+      if (!nextToLastNode) {
+        return;
+      }
+      // Grab the value from the tail
+      if (nextToLastNode.next) {
+        value = nextToLastNode.next.value;
+      }
+      // Set the next to last node as the new tail
+      // by wiping out it's next property
+      nextToLastNode.next = undefined;
+    }
+    list.length--;
+    return value;
+  }
 
-    export function removeFromHead<T>(list: LinkedList<T>): Optional<T> {
-        // If the list is empty do nothing
-        if (list.length === 0) {
-            return null
-        }
-        if (!list.head) {
-            return null
-        }
-        // grab the value from the current head
-        const value = list.head.value
-        // If there's only one node
-        if (list.length === 1) {
-            // set head  to null
-            list.head = null
-            // Otherwise we need to make next the new head
-        } else {
-            // Set the head to be the next node
-            list.head = list.head.next
-        }
-        // Decrement the length
-        list.length--
-        // Return the value
-        return value
+  export function insertValueAtIndex<T>(
+    list: LinkedList<T>,
+    index: number,
+    value: T,
+  ): void {
+    // if the list's length is less than the index, just return undefined
+    if (list.length < index) {
+      return;
     }
-
-    export function removeFromTail<T>(list: LinkedList<T>): Optional<T> {
-        // if there's no head, just return null
-        if (!list.head) {
-            return null;
-        }
-        let value: Optional<T> = null;
-        // If there's no next node
-        // We are removing the only node
-        if (!list.head.next) {
-            // grab the value from the head
-            value = list.head.value;
-            // and set head to null
-            list.head = null
-            // Otherwise we need to iterate to find the tail
-        } else {
-            // We can use our findNodeAtIndex here
-            // To look for the next to last node
-            const nextToLastNode = findNodeAtIndex(list, list.length - 2);
-            if (!nextToLastNode) {
-                return null
-            }
-            // Grab the value from the tail
-            if (nextToLastNode.next) {
-                value = nextToLastNode.next.value;
-            }
-            // Set the next to last node as the new tail
-            // by wiping out it's next property
-            nextToLastNode.next = null;
-        }
-        list.length--;
-        return value;
+    // If the index is 0,
+    // Just use our Head function
+    if (index === 0) {
+      addValueToHead(list, value);
+      return;
     }
-
-    export function insertValueAtIndex<T>(list: LinkedList<T>, index: number, value: T): null {
-        // if the list's length is less than the index, just return null
-        if (list.length < index) {
-            return null
-        }
-        // If the index is 0,
-        // Just use our Head function
-        if (index === 0) {
-            addValueToHead(list, value)
-            return null
-        }
-        // If the index is the last index,
-        // just use our tail function
-        if (index === list.length - 1) {
-            addValueToTail(list, value)
-        }
-        const node = findNodeAtIndex(list, index - 1)
-        if (!node) {
-            return null;
-        }
-        const newNode = createNode(value)
-        newNode.next = node.next
-        node.next = newNode
-        list.length++
-        return null
+    // If the index is the last index,
+    // just use our tail function
+    if (index === list.length - 1) {
+      addValueToTail(list, value);
     }
+    const node = findNodeAtIndex(list, index - 1);
+    if (!node) {
+      return;
+    }
+    const newNode = createNode(value);
+    newNode.next = node.next;
+    node.next = newNode;
+    list.length++;
+    return;
+  }
 }
